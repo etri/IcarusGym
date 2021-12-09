07e007cf3c036b9665d09d7d42054d45270cf70e
sjshin0505 <36652309+sjshin0505@users.noreply.github.com>
2021-12-09 23:43:48 +0900

    Create icarusgym sub-package

---
+++ icarusgym/__init__.py
@@ -0,0 +1,9 @@
+# Author: Seungjae Shin <sjshin0505@{etri.re.kr, gmail.com}>
+
+"""Package for using IcarusGym.
+"""
+
+from .actual_env import *
+from .envs import *
+from .execution import *
+from .models import *
